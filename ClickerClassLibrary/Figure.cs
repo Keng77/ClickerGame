897caa76fa9a87584425b034c54bef8aeae84b20
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-04-01 19:48:57 +0300

    Добавил класс Figure

---
+++ ClickerClassLibrary/Figure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresClassLibrary
+{
+    public abstract class Figure
+    {
+        public Pnt[] pnts = null;
+        public Color color { get; private set; } //свойство - цвет фигуры
+
+        public int TTL; //time to live
+        public Figure(Pnt[] pnts)
+        {
+            this.pnts = pnts;
+        }
+
+        public Figure(Pnt[] pnts, Color color, int ttl)
+        {
+            this.pnts = pnts;
+            this.color = color;
+            TTL = ttl;
+        }
+
+        public abstract bool isContain(Pnt pnt);
+
+        // Метод уменьшения
+        public void DecTTL()
+        {
+            TTL--;
+        }
+
+        // Проверка на существование
+        public bool isAlive()
+        {
+            if (TTL > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int GetCost()
+        {
+            return 5;
+        }
+    }
+}
