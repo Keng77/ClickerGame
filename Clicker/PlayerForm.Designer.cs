6da1033fb6658dbc067c97013aee71e62fb3b509
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-04-05 14:31:28 +0300

    Добавил форму  PlayerForm с вводом имён игроков

---
+++ Clicker/PlayerForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Clicker
+{
+    partial class PlayerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Player1TextBox = new System.Windows.Forms.TextBox();
+            this.Player2TextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 70);
+            this.label1.MaximumSize = new System.Drawing.Size(400, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the names of the players";
+            // 
+            // Player1TextBox
+            // 
+            this.Player1TextBox.Font = new System.Drawing.Font("MS PGothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Player1TextBox.Location = new System.Drawing.Point(90, 182);
+            this.Player1TextBox.Multiline = true;
+            this.Player1TextBox.Name = "Player1TextBox";
+            this.Player1TextBox.Size = new System.Drawing.Size(240, 38);
+            this.Player1TextBox.TabIndex = 1;
+            this.Player1TextBox.Text = "Player1";
+            // 
+            // Player2TextBox
+            // 
+            this.Player2TextBox.Font = new System.Drawing.Font("MS PGothic", 13.8F);
+            this.Player2TextBox.Location = new System.Drawing.Point(488, 182);
+            this.Player2TextBox.Multiline = true;
+            this.Player2TextBox.Name = "Player2TextBox";
+            this.Player2TextBox.Size = new System.Drawing.Size(240, 38);
+            this.Player2TextBox.TabIndex = 2;
+            this.Player2TextBox.Text = "Player2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS PGothic", 13.8F);
+            this.button1.Location = new System.Drawing.Point(190, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS PGothic", 13.8F);
+            this.button2.Location = new System.Drawing.Point(450, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Player2TextBox);
+            this.Controls.Add(this.Player1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "PlayerForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Player1TextBox;
+        private System.Windows.Forms.TextBox Player2TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ClickerClassLibrary/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickerClassLibrary
+{
+    internal class Player
+    {
+        public string name { get; private set; }
+        int score;
+
+        public Player(string name)
+        {
+            this.name = name;
+            score = 0;
+        }
+
+        public void AddScore(int score)
+        {
+            this.score += score;
+        }
+
+        public override string ToString()
+        {
+            return name + ": " + score;
+        }
+    }
+}
