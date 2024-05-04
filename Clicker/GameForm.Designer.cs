49f07a210a1ff4b8b8db2759d7b1cbfbb7bf4f00
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-05-04 17:55:10 +0300

    Добавил Диаграмму классов для фабричного метода

--- Clicker/GameForm.Designer.cs
+++ Clicker/GameForm.Designer.cs
@@ -86,7 +86,7 @@
             this.pictureBox2.InitialImage = null;
             this.pictureBox2.Location = new System.Drawing.Point(1472, 650);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(9, 14);
+            this.pictureBox2.Size = new System.Drawing.Size(12, 19);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Visible = false;
@@ -98,7 +98,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(360, 650);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(9, 14);
+            this.pictureBox1.Size = new System.Drawing.Size(12, 19);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
